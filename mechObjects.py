89524e35879d4b36f1c54e7832da8c536048424e
bryjan <bryanjaneliunas@yahoo.com>
2025-02-09 14:07:37 -0500

    Update mechObjects.py

--- mechObjects.py
+++ mechObjects.py
@@ -3,7 +3,7 @@ import gameClasses as C
 #MechModules do not touch Init stats
 
     #setModules
-radardetector = C.Module("Lancer Radar Detector", "PassiveRadar", 3, [False, False, True], desc="")
+radarDetector = C.Module("Lancer Radar Detector", "passiveRadar", 3, [False, False, True], desc="")
 
     #flatModules
 lightWeightFrame = C.Module("light Lancer Frame", "mp", 2, [-2 , -1, 2], desc= "A lightweight frame to increase mobility, at the cost of structural integrity")
@@ -13,11 +13,11 @@ exosuitLegMotor = C.Module("Exosuit Leg Motors", "mp", 1,  [.5 , .8 , 1], desc= 
 
 #MechLimbs
 greatHelm= C.Helm( "Great Helm", 5, 25, [], 6, 12, 50, desc = "A Lancer Power Armor Helm with a suite of sensor data displays inside")
-lancerPowerArmor= C.Hull( "Lancer Power Armor", 5, 25, [lightWeightFrame], 20, 1, 150, desc = "The namesake of the Lancer Class mechs. Not a true mech, but a very large set of power Armor. Extremely fast and agile compared to mechs of other classes, but lack data sensors, radar, and means of serious ranged damage.")
+lancerPowerArmor= C.Hull( "Lancer Power Armor", 5, 25, [lightWeightFrame], 20, 1, 150, 2, desc = "The namesake of the Lancer Class mechs. Not a true mech, but a very large set of power Armor. Extremely fast and agile compared to mechs of other classes, but lack data sensors, radar, and means of serious ranged damage.")
 lancerPowerArmorLegs= C.Legs( "Lancer Power Armor Legs", 5, 25, [exosuitLegMotor], 4, desc = "Exosuit powered legs covered in armor")
-godHelm= C.Helm( "God Helm", 5, 25,[radardetector], 10, 30, 150, desc = "Dev Tool")
-godHull= C.Hull( "God Hull", 5, 25, [], 100, 10, 50, desc = "Dev Tool")
-kikiLegs= C.Legs( "God Legs", 5, 25, [exosuitLegMotor, lightWeightFrame], 10, desc = "Dev Tool")
+godHelm= C.Helm( "God Helm", 5, 25,[radarDetector], 10, 15, 1000, desc = "Dev Tool")
+godHull= C.Hull( "God Hull", 5, 25, [], 50, 10, 50, 2, desc = "Dev Tool")
+kikiLegs= C.Legs( "God Legs", 5, 25, [exosuitLegMotor, lightWeightFrame], 11, desc = "Dev Tool")
 #MechClasses
 lancer = C.MechClass("Lancer Type.A" , "p", "dark_grey", lancerPowerArmor, greatHelm, lancerPowerArmorLegs, [], "")
 test = C.MechClass("Test" , "@", "yellow", godHull, godHelm, kikiLegs,[],"")
